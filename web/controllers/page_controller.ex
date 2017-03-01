7c0e1b01ad97ce07f565e904de88bd2b51791060
Peter-Fagan <peter_fagan@hotmail.com>
2017-03-01 11:37:49 +1100

    Add real descriptions

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -18,16 +18,20 @@ defmodule ElixirSydney.PageController do
         location: "Pivotal Labs, Level 11, 155 Clarence Street",
         location_url: "https://www.google.com.au/maps/dir//Pivotal,+11%2F155+Clarence+St,+Sydney+NSW+2000/@-33.8679869,151.1698764,13z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x6b12ae3f3b312871:0x40fe7bd0f94a2249!2m2!1d151.2049818!2d-33.8679919",
         description: """
-        Single-origin coffee selfies tbh, pabst hexagon farm-to-table ennui kogi neutra. 3 wolf moon umami art party, ethical flexitarian street art venmo. Vape hoodie wayfarers pop-up, artisan heirloom forage taxidermy. Venmo umami whatever, chicharrones activated charcoal pabst hammock prism master cleanse. VHS woke sartorial, tattooed glossier pug swag cray chicharrones PBR&B. Deep v woke swag, normcore viral photo booth actually. Typewriter cornhole meggings, forage ramps whatever godard art party gentrify mustache kickstarter PBR&B fashion axe.
+        This month we'll be talking about a topic that we haven't covered before, but is important for securing your webapps: Authentication with JWT and their usage with Plugs, Joken and Phoenix.
 
-        Cliche retro leggings vaporware vexillologist 90's. Ramps salvia taxidermy narwhal wayfarers. Meditation hoodie umami next level, gastropub keytar church-key man bun butcher pitchfork direct trade blue bottle pabst. Pinterest XOXO unicorn trust fund, microdosing subway tile church-key coloring book whatever hella tumblr la croix 90's ethical next level. Flannel bushwick sartorial asymmetrical flexitarian pinterest, heirloom locavore church-key raclette fam schlitz quinoa try-hard yuccie. Disrupt narwhal tacos iPhone mustache, vexillologist waistcoat hell of ennui fap jean shorts. Craft beer scenester listicle vinyl vape small batch.
+        We'll also be getting with writing Elixir in a recursive style. This may not be what you're used from other languages but it's pretty great in Elixir and Erlang. We'll learn how to write the ridiculously fast Elixir functions that are easy to read.
+
+        Lastly we'll learn about what's new in Elixir 1.4 with a deep dive into the new Process Registry module.
+
+        It's going to be a great meetup, so come along and join us!
         """,
         url: "https://www.meetup.com/en-AU/sydney-ex/events/237380598/",
         talks: [
           %Talk{
             title: "Authentication with JWT",
             description: """
-            Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.
+            Need to secure your Phoenix or Plug application? Not sure how JWT works? Well there are lots of options in Elixir for adding security to your webapp. Sunil will introduce JWT and the various options for add Authentication to your app.
             """,
             speaker: %Speaker{
               name: "Sunil Gopinath",
@@ -40,7 +44,9 @@ defmodule ElixirSydney.PageController do
           %Talk{
             title: "Getting Comfortable With Recursion",
             description: """
-            You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.
+            You might not be used to writing code in a recursive style if you're coming from a more imperative language. Heard of Tail Recursion or Tail Call Optimisation, but not sure what it really means?
+
+            Josh will demystify writing recursive functions that are blazingly fast and help you avoid some of the traps.
             """,
             speaker: %Speaker{
               name: "Josh Price",
@@ -53,7 +59,7 @@ defmodule ElixirSydney.PageController do
           %Talk{
             title: "Using the New Registry in Elixir 1.4",
             description: """
-            Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb.
+            Elixir 1.4 introduces the new Process Registry module to help manage your processes in OTP. If you've attended our OTP introductory series then you'll be ready to join Martin as he shows us how we can use this in our Elixir OTP applications.
             """,
             speaker: %Speaker{
               name: "Martin Stannard",
