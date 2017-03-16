0f599aa4a5ca615667b053a8ef6c315b92700b09
James Sadler <freshtonic@gmail.com>
2017-03-16 12:30:49 +1100

    Remove useless test

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirSydney.PageControllerTest do
-  use ElixirSydney.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
