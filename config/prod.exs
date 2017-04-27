58c4b3429384c773b97e9f4a869208fb2ba6da23
Josh Price <josh@canoniq.com>
2017-04-27 21:31:35 +1000

    Use url with host

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :elixir_sydney, ElixirSydney.Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  # url: [host: "elixir-sydney", port: 80],
+  url: [host: "elixir-sydney", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
